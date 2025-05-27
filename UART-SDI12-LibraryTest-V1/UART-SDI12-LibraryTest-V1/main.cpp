3635bc33818aaacddbd6579bc6f3b3efedd64b9e
Justin Widen <jdogwiden5@gmail.com>
2025-05-27 17:15:42 -0500

    Microchip Studio Testing Program
    Began work on a testing program in Microchip Studio using the USDI12 library.

--- UART-SDI12-LibraryTest-V1/UART-SDI12-LibraryTest-V1/main.cpp
+++ UART-SDI12-LibraryTest-V1/UART-SDI12-LibraryTest-V1/main.cpp
@@ -1,18 +1,29 @@
 /*
- * UART-SDI12-LibraryTest-V1.cpp
- *
- * Created: 5/20/2025 4:53:58 PM
- * Author : jwiden2
- */ 
+* UART-SDI12-LibraryTest-V1.cpp
+*
+* Created: 5/20/2025 4:53:58 PM
+* Author : jwiden2
+*/
 
-#include <avr/io.h>
+#define F_CPU 16000000UL
 
+//#include <avr/io.h>
+#include <util/delay.h>
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+#include "USDI12.hpp"
+
+int main(void) {
+	// - !EN_TX on PH2, EN_RX on PH3
+	USDI12 sdi12(&PORTH, (1 << PH2), &PORTH, (1 << PH3));
+
+	sdi12.set_rx();  // Initializes DDRs and sets RX mode
+	_delay_ms(100);
+
+	sdi12.set_tx();  // Switches to TX mode
+
+	while (1) {
+		// Toggle between modes, etc.
+	}
 }
+
 
