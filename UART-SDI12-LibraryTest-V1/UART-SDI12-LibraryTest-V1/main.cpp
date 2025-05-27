f79523af1364ffa1d55c0dc08ce74d435d928a4e
Justin Widen <jdogwiden5@gmail.com>
2025-05-27 11:50:18 -0500

    Added Microchip Studio project files for testing

---
+++ UART-SDI12-LibraryTest-V1/UART-SDI12-LibraryTest-V1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * UART-SDI12-LibraryTest-V1.cpp
+ *
+ * Created: 5/20/2025 4:53:58 PM
+ * Author : jwiden2
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
